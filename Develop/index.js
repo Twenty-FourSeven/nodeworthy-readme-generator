6de34861899a30fdbf5813d22deefd14a02cee82
unknown <thomas.james.1@hotmail.com>
2023-03-27 18:06:35 -0700

    add questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,3 @@
-// TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown')
@@ -7,15 +6,31 @@ inquirer.prompt([
     {
         type: "input",
         name: "title",
-        message: "What is the title of your project?"
+        message: "What is the title of your project?",
     },
     {
         type: "input",
         name: "description",
-        message: "what is the purpose or description of this project?"
+        message: "What is the purpose or description of this project?",
     },
-]).then(answers => {
-    fs.writeFile("README.md", generateMarkdown(answers), (err) => {
+    {
+        type: "input",
+        name: "description",
+        message: "What is the purpose or description of this project?",
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub user name?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
+
+]).then(data => {
+    fs.writeFile("README.md", generateMarkdown(data), (err) => {
         if (err) throw err;
         console.log("README file created!")
     })
